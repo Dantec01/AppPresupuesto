9d1048e818711b1785fde839dc6eb228f278183e
Daniel <danmeroja@gmail.com>
2021-12-03 21:52:22 -0400

    usuario 2

--- Usuario.java
+++ Usuario.java
@@ -30,15 +30,16 @@ public class Usuario
     // Este metodo es getter (devuelve un valor) pero
     // también es un setter (establece un valor)
     // Este método comprueba si el DNI es correcto
-    public boolean setEmailAddres(){ //(String emailAddres){
+    public void setEmailAddres(){ //(String emailAddres){
         //if(emailAddres.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
         if(email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
             this.email = email;
-            return true;
+            //return true;
             System.out.println("email correcto");
         }else{
-            return false;
+            //return false;
             System.out.println("email incorrecto");
         }
+        
     } 
 }
